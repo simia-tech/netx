7492591ec8910ea55007d09610d19df2e6ade76b
Philipp Brüll <pb@simia.tech>
2018-01-03 16:29:38 +0100

    added grpc helper function

--- grpc.go
+++ grpc.go
@@ -13,3 +13,10 @@ func NewGRPCDialer(network string, options ...value.Option) func(string, time.Du
 		return Dial(network, address, options...)
 	}
 }
+
+// NewGRPCMultiDialer returns a dialer that can be passed to the grpc.Dial function.
+func NewGRPCMultiDialer(md *MultiDialer) func(string, time.Duration) (net.Conn, error) {
+	return func(address string, timeout time.Duration) (net.Conn, error) {
+		return md.Dial(address)
+	}
+}
